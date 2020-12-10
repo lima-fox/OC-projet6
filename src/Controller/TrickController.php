5beeb818acb9ff87c1e7327905bc37cfd0182cd3
vagrant <vagrant@homestead>
2020-12-10 15:51:08 +0000

    add comment if user co + list comments

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Trick;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
 use App\Repository\TrickRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,13 +22,34 @@ class TrickController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    public function index(TrickRepository $trickRepository, int $id): Response
+    public function index(TrickRepository $trickRepository, int $id, Request $request, CommentRepository $commentRepository): Response
     {
         $trick = $trickRepository->find($id);
 
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $comment = $form->getData();
+
+            $comment->setUser($this->getUser());
+            $comment->setTrick($trick);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+        }
+
+        $comments = $commentRepository->findBy(['trick' => $trick], ['id' => 'desc'], 10 );
+
 
         return $this->render('trick/index.html.twig', [
-            'trick' => $trick
+            'trick' => $trick,
+            'form' => $form->createView(),
+            'comments' => $comments
         ]);
     }
 }
---
+++ src/Form/CommentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextType::class, [
+                'label' => 'Votre commentaire',
+                'constraints' => new Length(['min' => 3]),
+                'attr' => [
+                    'placeholder' => 'Ajouter votre commentaire'
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'Envoyer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
